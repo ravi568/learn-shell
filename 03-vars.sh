2dce2efe04b89bfaa66d2ec3fb17053604bfb1d3
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-18 17:47:41 +0530

    story 8- command and Arithemetic var dec

--- 03-vars.sh
+++ 03-vars.sh
@@ -4,3 +4,13 @@
 DATE=2023-02-18
 
 echo " hello, good morning, today date is ${DATE}"
+
+#command mode
+
+DATE=$(date)
+echo "Hello, good morning ,today date is ${DATE}"
+
+#Arithmetic exp
+ADD=$((3+3))
+
+echo " Addition of 3+3 = ${ADD}"
