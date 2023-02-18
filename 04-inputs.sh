8167437bdf2fd2dfb7bfe0de76bc3f3959e9a879
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-18 18:04:14 +0530

    story 8- inputs arguments

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -4,8 +4,8 @@ echo first argument $1 #First argument
 
 echo first argument $2 #Second argument
 
-echo all arguments $* = all arguments
+echo all arguments = $*
 
-echo all arguments $@ = all arguments
+echo all arguments = $@ # all arguments
 
 echo number of arguments = $# #number of arguments
