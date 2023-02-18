fa4a75c9f92ae214e16582ee8826f1b6269f2127
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-18 14:52:08 +0530

    story 5- color without reset statement

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,19 @@
 echo Helloworld
+
+## color syntax
+# echo -e "\e[COLm Message \e[0m"
+#echo -e ->enables color from echo command
+#\e[COLm -> enables a particular color
+# Color         Codes
+# Red           31
+# Green         32
+# Yellow        33
+# Blue          34
+# Magenta       35
+# Cyan          36
+
+# \e[0m -> zero code is going to reset the color ,menaing if we enable color for sure,we
+# need to disable it as well, otherwise the color will continue on screen.
+
+echo -e "\e[31m Helloworld in red color"
+echo helloworld
