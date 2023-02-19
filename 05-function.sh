a8519aff4a9b289296cdb141c2ab23b5541b0ace
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-19 14:53:26 +0530

    story 11- functions calling

--- 05-function.sh
+++ 05-function.sh
@@ -2,7 +2,13 @@
 
 xyz() {
   echo hello from function
+  echo first argument -$1
+  echo second argument -$2
+  echo all arguments - $*
+  echo no of arguments - $#
+
 }
 
+## main program
 ## call a function
-xyz
+xyz 123 456
