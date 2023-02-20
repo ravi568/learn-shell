c443ec790ddde9acc49389f5743faee6e88ed449
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-20 19:54:08 +0530

    story 17- if else conditions number and string

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,12 +1,13 @@
+
 number=$1
 string=$2
 
-if[ "${number}" -eq 5 ];  then
+if [ "${number}" -eq 5 ]; then # then can go in next line as well, also semi colan is also fine in the same line
   echo number is 5
+fi
 
 if [ "${string}" == abc ]; then
   echo string is abc
-
-  else
-    echo string is not abc
+else
+  echo string is not abc
 fi
