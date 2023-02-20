a7655b667d8b386bf9f1d0cf7cf352797af40b0c
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-02-20 19:48:18 +0530

    story 17- if else conditions number and string

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,10 +1,10 @@
 number=$1
 string=$2
 
-if[${number} -eq 5] ; then
+if[${number} -eq 5];  then
   echo number is 5
 
-if [${string} == abc] ; then
+if [${string} == abc]; then
   echo string is abc
 
   else
